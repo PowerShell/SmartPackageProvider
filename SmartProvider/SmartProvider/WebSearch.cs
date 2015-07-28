d6d2cb552dd178c915be97fba27632b7a4464898
Raghu Shantha <raghus@microsoft.com>
2015-07-28 14:39:03 -0700

    Added code to discover potential download locations

--- SmartProvider/SmartProvider/WebSearch.cs
+++ SmartProvider/SmartProvider/WebSearch.cs
@@ -1,14 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Text.RegularExpressions;
 
 namespace SmartProvider
 {
     class WebSearch
     {
         private PackageSource _source;
+
+        private String GetWebContent(string uri)
+        {
+            byte[] buf = new byte[8192];
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            Stream resStream = response.GetResponseStream();
+            string tempString = null;
+            StringBuilder sb = new StringBuilder();
+            int count = 0;
+            do
+            {
+                count = resStream.Read(buf, 0, buf.Length);
+
+                if (count != 0)
+                {
+                    tempString = Encoding.ASCII.GetString(buf, 0, count);
+                    sb.Append(tempString);
+                }
+            }
+            while (count > 0);
+
+            return sb.ToString();
+        }
+
+        private List<Uri> GetUrlIHtml(string uri, string patternPresent, string patternAbsent)
+        {
+            MatchCollection matches = Regex.Matches(this.GetWebContent(uri),
+                @"\b((https?|ftp|file)://|(www|ftp)\.)[-A-Z0-9+&@#/%?=~_|$!:,.;]*[A-Z0-9+&@#/%=~_|$]", RegexOptions.IgnoreCase);
+
+            List<Uri> packageDownloadLocation = new List<Uri>();
+
+            foreach (Match match in matches)
+            {
+                if (match.ToString().Contains(patternPresent) && !match.ToString().Contains(patternAbsent))
+                {
+                    Uri downloadUri = new Uri(match.Value.ToString());
+                    packageDownloadLocation.Add(downloadUri);
+                }
+            }
+
+            return packageDownloadLocation;
+        }
 
         public WebSearch(PackageSource source)
         {
@@ -19,7 +65,9 @@ namespace SmartProvider
         {
             // TODO: get top 3 links to download pages
             // using google search api/bing search api/website search box
-            return new List<Uri> { new Uri("https://notepad-plus-plus.org/download/"), new Uri("http://download.cnet.com/Notepad/3000-2352_4-10327521.html"), new Uri("http://notepad-plus.en.softonic.com/download"), new Uri("http://filehippo.com/download_notepad/") };
+            //return new List<Uri> { new Uri("https://notepad-plus-plus.org/download/"), new Uri("http://download.cnet.com/Notepad/3000-2352_4-10327521.html"), new Uri("http://notepad-plus.en.softonic.com/download"), new Uri("http://filehippo.com/download_notepad/") };
+
+            return this.GetUrlIHtml("http://google.com/search?q=" + name + "%20download%20location", "/download", "google");
         }
     }
 }
