c0eaf37bcb0e897d68227086b82602c364070d7b
Raghu Shantha <raghus@microsoft.com>
2015-07-28 14:43:39 -0700

    Added code to discover potential download locations

--- SmartProvider/SmartProvider/WebSearch.cs
+++ SmartProvider/SmartProvider/WebSearch.cs
@@ -63,8 +63,6 @@ namespace SmartProvider
 
         public List<Uri> Search(string name)
         {
-            // TODO: get top 3 links to download pages
-            // using google search api/bing search api/website search box
             //return new List<Uri> { new Uri("https://notepad-plus-plus.org/download/"), new Uri("http://download.cnet.com/Notepad/3000-2352_4-10327521.html"), new Uri("http://notepad-plus.en.softonic.com/download"), new Uri("http://filehippo.com/download_notepad/") };
 
             return this.GetUrlIHtml("http://google.com/search?q=" + name + "%20download%20location", "/download", "google");
