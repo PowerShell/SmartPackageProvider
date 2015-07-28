de0bad16d527ae8ecf0f4ed3d19df4838ac08348
Darek Grala <dagrala@microsoft.com>
2015-07-28 14:05:08 -0700

    Added meta refresh support, checking if Url point to an actual exe

--- SmartProvider/SmartProvider/DownloadLinkFinder.cs
+++ SmartProvider/SmartProvider/DownloadLinkFinder.cs
@@ -35,7 +35,11 @@ namespace SmartProvider
                 {
                     if (null != bestExeLink.Attributes["href"])
                     {
-                        return new Uri(uri, bestExeLink.Attributes["href"].Value);
+                        var potentialUrl = new Uri(uri, bestExeLink.Attributes["href"].Value);
+                        if (await CheckPotentialExe(potentialUrl))
+                        {
+                            return potentialUrl;
+                        }
                     }
                     return null;
                 }
@@ -48,10 +52,65 @@ namespace SmartProvider
                 if (bestDownloadNowLink != null)
                 {
                     var redirectLink = new Uri(uri, (null != bestDownloadNowLink.Attributes["href"]) ? bestDownloadNowLink.Attributes["href"].Value : null);
+                    if (await CheckPotentialExe(redirectLink))
+                    {
+                        return redirectLink;
+                    }
+                    else
+                    {
+                        // THIRD, check for meta refresh
+                        response = await httpClient.GetAsync(uri);
+
+                        //will throw an exception if not successful
+                        response.EnsureSuccessStatusCode();
+
+                        content = await response.Content.ReadAsStringAsync();
+
+                        htmlDoc = new HtmlDocument();
+                        htmlDoc.LoadHtml(content);
+
+                        if (htmlDoc.DocumentNode != null)
+                        {
+                            // search for meta refresh
+                            var metaRefresh = htmlDoc.DocumentNode.SelectNodes("//meta[contains(@http-equiv,'refresh')]");
+                            if (metaRefresh != null && metaRefresh.Count > 0 && metaRefresh[0] != null)
+                            {
+                                var metaRefreshLink = metaRefresh[0].GetAttributeValue("url", null);
+                                var metaRefreshUri = new Uri(metaRefreshLink);
+                                if (await CheckPotentialExe(metaRefreshUri))
+                                {
+                                    return metaRefreshUri;
+                                }
+                            }
+                        }
+                    }
                 }
             }
 
             return null;
+        }
+
+        private async static Task<bool> CheckPotentialExe(Uri uri)
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(uri);
+
+            //will throw an exception if not successful
+            response.EnsureSuccessStatusCode();
+
+            //string content = await response.Content.ReadAsStringAsync();
+
+            IEnumerable<string> values;
+            if (response.Content.Headers.TryGetValues("Content-Type", out values))
+            {
+                var contentType = values.First();
+                if (contentType == "application/x-msdos-program")
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }
